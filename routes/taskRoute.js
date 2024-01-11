c3e5836a4d409bb14f087301ee97e233f5f1fd68
Muhammad Faran Ayyaz <faranayyaz@gmail.com>
2024-01-11 12:25:25 +0200

    tweak

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -5,6 +5,7 @@ const { isAuthenticated } = require("../middlewares/authenticated");
 const taskController = require("../controllers/taskController");
 
 // API endpoint to enqueue requests
+// router.post("/enqueue", isAuthenticated, processFileUpload, taskController.enqueueTask); // use this one as we require authentication
 router.post("/enqueue", processFileUpload, taskController.enqueueTask);
 
 module.exports = router;
